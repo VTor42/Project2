1a42835b66afae01ff10d8403ca41ba2e3b65664
Viktoria Grossman <vgrossman@putneyschool.org>
2017-01-17 10:45:45 -0500

    recursion commit

--- FibinacciSequenceRecursion.java
+++ FibinacciSequenceRecursion.java
@@ -1,31 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package fibinacci.sequence.recursion;
 
-/**
- *
- * @author viktoriagrossman
- */
 public class FibinacciSequenceRecursion {
 
     public static void main(String[] args) {
-   
-    /**
-     *
-     * @param n
-     * @return
-     */
+      fibonacci(10);
+    }
     
-public static int fibonacci(int n){
-        if(n == 0 || n==2)
+    public static int fibonacci(int n){
+        if(n == 0){
             return 0;
-            return 1;   
+        }
+        if(n == 1){
+            return 1;  
+        }
+        int result = fibonacci(n-2)+fibonacci(n-1);
+        System.out.println(result);
+        return result;         
     }
-     return fibonacci(n-1);
-    }
+     
+}
 
  
    
